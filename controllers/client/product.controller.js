895103a52e084cd50c98822366c226f56637e1bb
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2025-11-08 18:12:24 +0700

    Hiển thị sản phẩm bên Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,19 @@
-module.exports.product = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.product = async (req, res) => {
+  // Đoạn này lấy ra database
+  const products = await Product.find({
+    status: "active",
+    deleted: "false"
+  });
+
+  const newProducts = products.map(item => {
+    item.priceNew = (item.price*(100 - item.discountPercentage)/100).toFixed(0)
+    return item;
+  })
+
   res.render("client/pages/products/index", {
     pageTitle: "Danh sách sản phẩm fff",
+    products: newProducts // gửi dữ liệu qua client/pages/products/index
   });
 };

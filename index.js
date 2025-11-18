d5a6a72019977ec8b44b3f2951a28aaca8fe826d
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2025-11-19 00:58:06 +0700

    TinyMCE trình soạn thảo

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 const express = require("express"); // import
 // Cấu hình env
-require('dotenv').config()
+require("dotenv").config();
 // console.log(process.env.PORT)
 
+// import path để dùng tinyMCE
+const path = require("path");
+
 // import mongoose
-const database = require("./config/database")
+const database = require("./config/database");
 database.connect();
 
 // import routes client
-const route = require("./routes/client/index.route")
+const route = require("./routes/client/index.route");
 
 // import routes admin
-const routeAdmin = require("./routes/admin/index.route")
+const routeAdmin = require("./routes/admin/index.route");
 
 // import thằng method-override
-var methodOverride = require('method-override')
+var methodOverride = require("method-override");
 
 const app = express(); // toàn bộ chương trình
 
 // import thằng method-override (ghi sau biến app)
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
 // import bodyparser
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 // import thư viện multer
-const multer  = require('multer')
+const multer = require("multer");
 
 // import thư viện express-flash
-const flash = require('express-flash')
-const cookieParser = require('cookie-parser')
-const session = require("express-session")
+const flash = require("express-flash");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 const port = process.env.PORT; // port = 3000
 
@@ -40,8 +43,8 @@ app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
 // Khởi tạo Flash
-app.use(cookieParser('keyboard cat')); // Đọc cookie session ID từ trình duyệt
-app.use(session({ cookie: { maxAge: 60000 }})); // middleware lưu dữ liệu session trên server, cookie giúp liên kết session với clien
+app.use(cookieParser("keyboard cat")); // Đọc cookie session ID từ trình duyệt
+app.use(session({ cookie: { maxAge: 60000 } })); // middleware lưu dữ liệu session trên server, cookie giúp liên kết session với clien
 app.use(flash()); // Dùng session này để lưu thông báo tạm thời
 /**
  * Flash Message (thông báo tạm thời) không lưu trực tiếp trên cookie
@@ -49,19 +52,25 @@ app.use(flash()); // Dùng session này để lưu thông báo tạm thời
  * Dữ liệu message thực sự nằm trên server, cookie chỉ là cầu nối
  */
 // Biến toàn cục
-const systemConfig = require("./config/system")
+const systemConfig = require("./config/system");
 // Đặt tên biến là prefixAdmin
-app.locals.prefixAdmin = systemConfig.prefixAdmin // /ADMIN
+app.locals.prefixAdmin = systemConfig.prefixAdmin; // /ADMIN
 
-app.use(bodyParser.urlencoded())  // nên khai báo th này trước route
+// TinyMCE
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+
+app.use(bodyParser.urlencoded()); // nên khai báo th này trước route
 
 // Routes
-route(app)
-routeAdmin(app)
+route(app);
+routeAdmin(app);
 
 // Nhúng file tĩnh: các file có thể xem được ở bên ngoài
 // app.use(express.static('public')) // public để o ffline được nhưng online sẽ lỗi
-app.use(express.static(`${__dirname}/public`)) // biến này chạy đc cả local cả online
+app.use(express.static(`${__dirname}/public`)); // biến này chạy đc cả local cả online
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ public/admin/js/tinymce-config.js
@@ -0,0 +1,6 @@
+tinymce.init({
+  selector: 'textarea', // áp dụng với tất cả thẻ area. còn nếu chỉ áo dụng với các class thì selector: 'textarea.textarea-mce'
+  plugins: 'image'
+});
+
+// Dùng bản v5 

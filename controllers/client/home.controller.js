823c347e8c4ffac7ac1c665bdc8fe09f89c7e58a
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2025-11-25 23:10:49 +0700

    CLIENT Hiển thị danh sách sản phẩm nổi bât

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,9 +1,19 @@
 // Nhận request từ người dùng, xử lý logic, rồi quyết định render ra trang nào / trả dữ liệu nào.
 const ProductCategory = require("../../models/product-category.model");
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
 
 module.exports.home = async (req, res) => {
-  
+  // Lấy ra sản phẩm nổi bật
+  const productsFeatured = await Product.find({
+    featured: "1",
+    deleted: false,
+    status: "active",
+  }).limit(1); // láy ra bao nhiểu sản phẩm nổi bật thì set limit
+  const newProducts = productsHelper.priceNewProducts(productsFeatured);
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
+    productsFeatured: newProducts,
   });
 };

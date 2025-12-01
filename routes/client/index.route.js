11a3015a000114fdebc1e446fc866fc0e57305f2
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2025-12-01 22:44:19 +0700

    CLIENT Đăng  nhập tài khoản

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,6 +1,9 @@
 // Định nghĩa đường dẫn gọi controller nào
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 const cartMiddleware = require("../../middlewares/client/cart.middleware")
+const userMiddleware = require("../../middlewares/client/user.middleware")
+
+
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
 const searchRoutes = require("./search.route");
@@ -11,6 +14,7 @@ const userRoutes = require("./user.route");
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
   app.use(cartMiddleware.cartId);
+  app.use(userMiddleware.infoUser);
 
   app.use("/", homeRoutes);
 
--- validates/client/user.validate.js
+++ validates/client/user.validate.js
@@ -18,3 +18,18 @@ module.exports.registerPost = async (req, res, next) => {
 
   next();
 };
+
+module.exports.loginPost = async (req, res, next) => {
+  if (!req.body.email) {
+    req.flash("error", "Vui lòng nhập email! ");
+    res.redirect(req.get("Referer"));
+    return;
+  }
+  if (!req.body.password) {
+    req.flash("error", "Vui lòng nhập mật khẩu!  ");
+    res.redirect(req.get("Referer"));
+    return;
+  }
+
+  next();
+};

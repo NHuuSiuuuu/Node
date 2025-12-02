9fc0a5da8971c5020cbbb7650f0f94e34c906013
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2025-12-02 23:55:24 +0700

    Trang dashbroad bên ADMIN

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,8 +1,49 @@
 // Nhận request từ người dùng, xử lý logic, rồi quyết định render ra trang nào / trả dữ liệu nào.
 //  [GET] /admin/dashboard
+const ProductCategory = require("../../models/product-category.model");
 
-module.exports.dashboard = (req, res) => {
-  res.render("admin/pages/dashboard/dashboard.pug",{
-    pageTitle: "Trang chủ"
-  })
+module.exports.dashboard = async (req, res) => {
+  // Thông kê
+  const statistic = {
+    categoryProduct: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    product: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    account: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    user: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+  };
+
+  statistic.categoryProduct.total = await ProductCategory.countDocuments({
+    deleted: false,
+  });
+
+  statistic.categoryProduct.active = await ProductCategory.countDocuments({
+    status: "active",
+    deleted: false,
+  });
+
+  statistic.categoryProduct.inactive = await ProductCategory.countDocuments({
+    status: "inactive",
+    deleted: false,
+  });
+
+  
+  res.render("admin/pages/dashboard/dashboard.pug", {
+    pageTitle: "Trang chủ",
+    statistic: statistic,
+  });
 };

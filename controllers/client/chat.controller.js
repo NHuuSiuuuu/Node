ed8afba6498a3bd99403d4a916ee52890c11870b
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2025-12-06 22:28:03 +0700

    Tối ưu lại Folder SocketIO

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,55 +1,12 @@
 const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
-const uploadToCloudinary = require("../../helpers/uploadCloudinary");
+
+const chatSocket = require("../../sockets/client/chat.socket");
 
 module.exports.index = async (req, res) => {
-  const userId = res.locals.user.id;
-  const fullName = res.locals.user.fullName;
-
-  // Socket IO
-  // Lắng nghe sự kiện gửi tin nhắn lên server
-  // Dùng on mỗi lần reload lại trang web sẽ tạo 1 socket mới khiến nó nó tạo nhiều bản ghi trong db
-  // Thay vào đấy dùng once
-  _io.once("connection", (socket) => {
-    // console.log("a user connected", socket.id);
-    // Đoạn code này lắng nghe gửi tin nhắn lên
-    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
-      let images = [];
-      for (const imageBuffer of data.images) {
-        const link = await uploadToCloudinary(imageBuffer);
-        images.push(link);
-      }
-      console.log(images);
-      const chat = new Chat({
-        user_id: userId,
-        content: data.content,
-        image: images, // nhở fix image bên model phải là images
-      });
-      await chat.save();
-
-      // //  Trả data về client
-      _io.emit("SERVER_RETURN_MESSAGE", {
-        userId: userId,
-        fullName: fullName,
-        content: data.content,
-        image: images,
-      });
-    });
-
-    // Đoạn code này lắng nghe gửi typing lên
-    // Typing
-    socket.on("CLIENT_SEND_TYPING", async (type) => {
-      socket.broadcast.emit("SERVER_RETURN_TYPING", {
-        userId: userId,
-        fullName: fullName,
-        type: type,
-      });
-    });
-
-    // End Typing
-  });
-
-  // End Socket IO
+  // SocketIO
+  chatSocket(res);
+  // End SocketIO
 
   // Lấy data ừ database
   const chats = await Chat.find({

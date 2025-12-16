27f19f8a9c9a813cdb358aaa7aa741764002a3bd
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2025-12-16 23:52:27 +0700

    Tạo model  phòng chat

---
+++ models/rooms-chat.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const roomChatSchema = new mongoose.Schema(
+  {
+    title: String,
+    avatar: String,
+    typeRoom: String, // type friend or group
+    status: String,
+    users: {
+      user_id: String,
+      role: String, // admin or user or superAdmin
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const RoomChat = mongoose.model("RoomChat", roomChatSchema, "rooms-chat");
+
+module.exports = RoomChat;
